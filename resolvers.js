849c88d3be791a2d3473855cb83be955c0a76fd1
Badri <badri.py@outlook.com>
2019-10-09 20:10:25 +0530

    Add the first mutation

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,19 @@
 exports.resolvers = {
     Query: {
         getAllRecipes: () => {}
+    },
+    Mutation: {
+        // Method gets param(root, args, context)
+        addRecipe: async (root, { name, category, description, instructions, userName}, {Recipe}) => {
+            const newRecipe = await new Recipe({
+                name,
+                category,
+                description,
+                instructions,
+                userName
+            }).save();
+
+            return newRecipe;
+        }
     }
 };
