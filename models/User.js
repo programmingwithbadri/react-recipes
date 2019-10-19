e607879e9355845ee59b3b83623a7597fcfda179
Badri <badri.py@outlook.com>
2019-10-19 16:43:38 +0530

    encrypts the password when storing in DB using BCRYPT

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -27,6 +28,27 @@ const UserSchema = Schema({
         type: [Schema.Types.ObjectId],
         ref: 'Recipe'
     }
+})
+
+// Runs this function before the data is being saved to the DB
+UserSchema.pre('save', function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err) {
+            return next(err);
+        }
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if(err) {
+                return next(err);
+            }
+            
+            this.password = hash;
+            next();
+        })
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema);
