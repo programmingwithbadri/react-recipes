73efacf171bd818a2eb6ad4dbd6da1c612aebe56
Badri <badri.py@outlook.com>
2019-10-19 17:05:48 +0530

    add the signup form

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export default class SignUp extends Component {
     render() {
         return (
-            <div>
-                SignUp
+            <div className="App">
+                <h2 className="App">SignUp</h2>
+                <form className="form">
+                    <input type="text" name="username" placeholder="User Name" />
+                    <input type="email" name="email" placeholder="Email Address" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" />
+                    <button type="submit" className="button-primary">Submit</button>
+                </form>
             </div>
         )
     }
