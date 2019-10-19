34487c2016e362e801bee0d740db9b2e7d93e156
Badri <badri.py@outlook.com>
2019-10-19 16:55:42 +0530

    add the routes for the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4444/graphql'
 });
 
+const Root = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Redirect to="/" />
+        </Switch>
+    </BrowserRouter>
+)
+
 ReactDOM.render(
-<ApolloProvider client = {client}>
-   <App />
-</ApolloProvider>, document.getElementById('root'));
+    <ApolloProvider client={client}>
+        <Root />
+    </ApolloProvider>, document.getElementById('root'));
 
