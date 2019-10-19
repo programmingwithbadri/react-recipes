e1b7ada6158ecf7905ec62d6163550f29775dcc0
Badri <badri.py@outlook.com>
2019-10-19 17:40:24 +0530

    Add on submit method for submitting the form

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -4,7 +4,7 @@ import { SIGNUP_USER } from '../../queries';
 
 export default class SignUp extends Component {
     state = {
-        username: "",
+        userName: "",
         email: "",
         password: "",
         confirmPassword: ""
@@ -17,16 +17,26 @@ export default class SignUp extends Component {
         })
 
     }
+
+    handleSubmit = (event, signUpUser) => {
+        event.preventDefault();
+        signUpUser().then(data => {
+            console.log(data)
+        })
+
+    }
+
     render() {
-        const { username, email, password, confirmPassword } = this.state;
+        const { userName, email, password, confirmPassword } = this.state;
         return (
             <div className="App">
                 <h2 className="App">SignUp</h2>
-                <Mutation mutation={SIGNUP_USER}>
-                    {() => {
+                <Mutation mutation={SIGNUP_USER}
+                    variables={{ userName, email, password }}>
+                    {(signUpUser, { data, loading, error }) => {
                         return (
-                            <form className="form">
-                                <input type="text" name="username" onChange={this.handleChange} placeholder="User Name" value={username} />
+                            <form className="form" onSubmit={event => this.handleSubmit(event, signUpUser)}>
+                                <input type="text" name="userName" onChange={this.handleChange} placeholder="User Name" value={userName} />
                                 <input type="email" name="email" onChange={this.handleChange} placeholder="Email Address" value={email} />
                                 <input type="password" name="password" onChange={this.handleChange} placeholder="Password" value={password} />
                                 <input type="password" name="confirmPassword" onChange={this.handleChange} placeholder="Confirm Password" value={confirmPassword} />
