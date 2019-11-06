c71295c518b6ee62586cda4e2898f3c591127039
Badri <badri.py@outlook.com>
2019-11-06 20:29:31 +0530

    Show recipe info in the recipe page

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -14,7 +14,17 @@ const Recipe = (props) => {
                 if (loading) return <div>Loading...</div>;
                 if (error) return <div>Error!</div>;
                 console.log(data);
-                return <div >Recipe page</div>;
+                return (
+                    <div className="App">
+                        <h2>{data.getRecipe.name}</h2>
+                        <p>Category: {data.getRecipe.category}</p>
+                        <p>Description: {data.getRecipe.description}</p>
+                        <p>Instructions: {data.getRecipe.instructions}</p>
+                        <p>Likes: {data.getRecipe.likes}</p>
+                        <p>Created By: {data.getRecipe.username}</p>
+                        <button>Like</button>
+                    </div>
+                );
             }}
         </Query >
     )
