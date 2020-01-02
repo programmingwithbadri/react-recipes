fa7a575217e173391f44e77756f0c5cb5c4e842a
Badri <badri.py@outlook.com>
2020-01-02 23:48:58 +0530

    Develop Client-side Logic to Properly Toggle Like

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -5,30 +5,56 @@ import { LIKE_RECIPE } from '../../queries';
 
 class LikeRecipe extends Component {
     state = {
+        liked: false,
         userName: ''
     }
 
     componentDidMount() {
         if (this.props.session.getCurrentUser) {
-            const { userName } = this.props.session.getCurrentUser;
-            this.setState({ userName });
+            const { userName, favourites } = this.props.session.getCurrentUser;
+            const { _id } = this.props;
+
+            // Check if the current recipe is already part of the favourites(liked) recipes
+            const prevLiked = favourites.findIndex(favourite => favourite._id === _id) > -1;
+
+            this.setState({
+                liked: prevLiked,
+                userName
+            });
         }
     }
 
-    handleLike = (likeRecipe) => {
-        likeRecipe().then(({ data }) => {
-            console.log(data)
-        })
+    handleClick = likeRecipe => {
+        // Toggle the liked state if the button is clicked
+        this.setState(prevState => ({
+            liked: !prevState.liked
+        }),
+            () => this.handleLike(likeRecipe)
+        )
+    }
+
+    handleLike = likeRecipe => {
+        if (this.state.liked) {
+            likeRecipe().then(async ({ data }) => {
+                console.log(data);
+                await this.props.refetch();
+            })
+        } else {
+            // mutation for unlike the recipe
+            console.log('Unlike')
+        }
     }
 
     render() {
-        const { userName } = this.state;
+        const { userName, liked } = this.state;
         const { _id } = this.props;
         return (
             <Mutation mutation={LIKE_RECIPE} variables={{ _id, userName }}>
                 {likeRecipe => (
                     userName
-                    && <button onClick={() => this.handleLike(likeRecipe)}>Like</button>
+                    && <button onClick={() => this.handleClick(likeRecipe)}>
+                        {liked ? 'Liked' : 'Like'}
+                    </button>
                 )}
             </Mutation>
         )
