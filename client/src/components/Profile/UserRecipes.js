e1745dd05f5fbe0875fec7b48ca3a5cffa2200f1
Badri <badri.py@outlook.com>
2020-01-02 23:00:14 +0530

    Provide Default Text for User Without Recipes

--- client/src/components/Profile/UserRecipes.js
+++ client/src/components/Profile/UserRecipes.js
@@ -22,55 +22,57 @@ export const UserRecipes = ({ userName }) => {
                 return (
                     <ul className="App">
                         <h3>Your Recipe's:</h3>
-                        {
-                            data.getUserRecipes.map(recipe => (
-                                <li key={recipe._id}>
-                                    <Link to={`/recipe/${recipe._id}`}>
-                                        <p>{recipe.name}</p>
-                                    </Link>
-                                    <p style={{ marginBottom: '0' }}>Likes: {recipe.likes}</p>
-                                    <Mutation
-                                        mutation={DELETE_USER_RECIPE}
-                                        variables={{ _id: recipe._id }}
-                                        // the below queries will be run so that
-                                        // it will be update in home page
-                                        refetchQueries={() => [
-                                            { query: GET_ALL_RECIPES },
-                                            { query: GET_CURRENT_USER }
-                                        ]}
-                                        update={(cache, { data: { deleteUserRecipe } }) => { // destructuring deleteUserRecipe
-                                            const { getUserRecipes } = cache.readQuery({
-                                                query: GET_USER_RECIPES,
-                                                variables: { userName }
-                                            }); // get all user recipes from the cache
+                        {!data.getUserRecipes.length &&
+                            <p><strong>You have not added any recipes yet</strong></p>
+                        }
+                        {data.getUserRecipes.map(recipe => (
+                            <li key={recipe._id}>
+                                <Link to={`/recipe/${recipe._id}`}>
+                                    <p>{recipe.name}</p>
+                                </Link>
+                                <p style={{ marginBottom: '0' }}>Likes: {recipe.likes}</p>
+                                <Mutation
+                                    mutation={DELETE_USER_RECIPE}
+                                    variables={{ _id: recipe._id }}
+                                    // the below queries will be run so that
+                                    // it will be update in home page
+                                    refetchQueries={() => [
+                                        { query: GET_ALL_RECIPES },
+                                        { query: GET_CURRENT_USER }
+                                    ]}
+                                    update={(cache, { data: { deleteUserRecipe } }) => { // destructuring deleteUserRecipe
+                                        const { getUserRecipes } = cache.readQuery({
+                                            query: GET_USER_RECIPES,
+                                            variables: { userName }
+                                        }); // get all user recipes from the cache
 
-                                            // update the cache to remove the deleted recipe 
-                                            // so that we dont need to reload the page
-                                            // to not see the deleted recipe
-                                            cache.writeQuery({
-                                                query: GET_USER_RECIPES,
-                                                variables: { userName },
-                                                data: {
-                                                    getUserRecipes: getUserRecipes.filter(
-                                                        recipe => recipe._id !== deleteUserRecipe._id
-                                                    )
-                                                }
-                                            });
-                                        }}
-                                    >
-                                        {(deleteUserRecipe, attrs = {}) => { // attrs contains the additional functions to find if the operation is loading
-                                            return (
-                                                <p
-                                                    className="delete-button"
-                                                    onClick={() => deleteRecipe(deleteUserRecipe)}
-                                                >
-                                                    {attrs.loading ? "deleting..." : "X"}
-                                                </p>
-                                            )
-                                        }}
-                                    </Mutation>
-                                </li>
-                            ))
+                                        // update the cache to remove the deleted recipe 
+                                        // so that we dont need to reload the page
+                                        // to not see the deleted recipe
+                                        cache.writeQuery({
+                                            query: GET_USER_RECIPES,
+                                            variables: { userName },
+                                            data: {
+                                                getUserRecipes: getUserRecipes.filter(
+                                                    recipe => recipe._id !== deleteUserRecipe._id
+                                                )
+                                            }
+                                        });
+                                    }}
+                                >
+                                    {(deleteUserRecipe, attrs = {}) => { // attrs contains the additional functions to find if the operation is loading
+                                        return (
+                                            <p
+                                                className="delete-button"
+                                                onClick={() => deleteRecipe(deleteUserRecipe)}
+                                            >
+                                                {attrs.loading ? "deleting..." : "X"}
+                                            </p>
+                                        )
+                                    }}
+                                </Mutation>
+                            </li>
+                        ))
                         }
                     </ul>)
             }}
